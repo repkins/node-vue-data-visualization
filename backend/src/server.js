655940337365a4bfe32624181e6a43f9af15f677
Antons Repins <dumperix1993@gmail.com>
2024-10-27 17:51:36 +0200

    refactor: extraction of socket connection handler into own function

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,19 +6,25 @@ const webSocketServerOptions = {
 };
 
 const wss = new WebSocketServer(webSocketServerOptions, () => {
-    console.log(`WebSocket Server listening on ${webSocketServerOptions.port}`);
+    console.log(`WebSocket Server listening on ${wss.options.port}`);
 });
+
+wss.on('connection', onSocketConnection);
 
 const numberGenerator = new NumberGenerator(Math.random, setInterval);
 
-wss.on('connection', ws => {
-
+/**
+ * Handles new connections to WebSocket server
+ * @param {WebSocket} ws Newly connected client socket
+ */
+function onSocketConnection(ws) 
+{
     const onNumber = number => {
         ws.send(number);
     };
 
     numberGenerator.on('number', onNumber);
-    
+
     ws.on('error', err => {
         console.error(err);
 
@@ -26,10 +32,11 @@ wss.on('connection', ws => {
     });
 
     ws.on('close', () => {
-        numberGenerator.off('number', onNumber)
+        numberGenerator.off('number', onNumber);
     });
-    
+
     ws.on('message', msg => {
         console.log('received: %s', msg);
     });
-});
+}
+
