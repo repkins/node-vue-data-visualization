6dd8817b85d474b889d398904490804ef9cfd284
Antons Repins <dumperix1993@gmail.com>
2024-10-29 13:59:12 +0200

    feat: frontend code initialized

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+    import TheWelcome from './components/TheWelcome.vue'
+
+    export default {
+        components: {
+            TheWelcome
+        }
+    }
+</script>
+
+<template>
+    <div class="container py-4 px-3 mx-auto">
+        <TheWelcome></TheWelcome>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/TheWelcome.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            increment() {
+                this.count++;
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>Hello, Bootstrap and Vite!</h1>
+    <button class="btn btn-primary" @click="increment()">{{ count }}</button>
+</template>
