95ef1ba60726f30b2e49028950262198afd3ecfe
Antons Repins <dumperix1993@gmail.com>
2024-11-01 16:17:31 +0200

    feat: default backend host if not specified in .env

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { Dashboard, DashboardKey } from './services/dashboard';
 
+const ApiHost = import.meta.env.VITE_API_HOST ?? 'localhost:8080';
+
 createApp(App)
-  .provide(DashboardKey, new Dashboard(import.meta.env.VITE_API_HOST))
+  .provide(DashboardKey, new Dashboard(ApiHost))
   .mount('#app');
